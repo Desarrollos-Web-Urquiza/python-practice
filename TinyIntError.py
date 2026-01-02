359edd9c7e0c3abf72f61e5dcefcd6816689a4d1
Desarrollos Web Urquiza <desarrollos_web_urquiza@hotmail.com>
2026-01-02 16:02:43 -0300

    updating the TinyIntError.py file

--- TinyIntError.py
+++ TinyIntError.py
@@ -5,11 +5,23 @@ class TinyIntError(Exception):
     def  __str__(self):
         return self.message
     
-def tiny_int(val):
+def validate_tiny_int(val):
     return val >= 0 and val <= 255
 
-def validate_tiny_int(val): #"60"
-    return isinstance(val, int)
+def validate_val(val): #"60"
+    try:
+        return isinstance(int(val), int)
+    except ValueError as error:
+        return False
+
+def tiny_int(val):
+    try:
+        if validate_val(val) and validate_tiny_int(val):
+            return True
+        else:
+            raise TinyIntError()
+    except TinyIntError as error:
+        print(error)
 
 try:
     numero = 400 
@@ -17,5 +29,8 @@ try:
         print ("El número es correcto")
     else: 
         raise TinyIntError()
+    
 except TinyIntError as error:
     print(error)
+
+print(tiny_int(254))
