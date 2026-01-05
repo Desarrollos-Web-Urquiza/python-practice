0b6016a21504415ed00abdb670676b1e6465dea4
Desarrollos-Web-Urquiza <desarrollos_web_urquiza@hotmail.com>
2026-01-05 16:40:07 -0300

    adding a new package

--- TinyIntError.py
+++ TinyIntError.py
@@ -1,36 +1,4 @@
-class TinyIntError(Exception):
-    def __init__(self):
-        self.message = 'El número no cuenta con las caracteristicas de un número tinyInt'
+from tinyIntError import tiny_int
+from tinyIntError import call_back_function
 
-    def  __str__(self):
-        return self.message
-    
-def validate_tiny_int(val):
-    return val >= 0 and val <= 255
-
-def validate_val(val): #"60"
-    try:
-        return isinstance(int(val), int)
-    except ValueError as error:
-        return False
-
-def tiny_int(val):
-    try:
-        if validate_val(val) and validate_tiny_int(val):
-            return True
-        else:
-            raise TinyIntError()
-    except TinyIntError as error:
-        print(error)
-
-try:
-    numero = 400 
-    if tiny_int(numero): 
-        print ("El número es correcto")
-    else: 
-        raise TinyIntError()
-    
-except TinyIntError as error:
-    print(error)
-
-print(tiny_int(254))
+print(tiny_int(55, call_back_function))
